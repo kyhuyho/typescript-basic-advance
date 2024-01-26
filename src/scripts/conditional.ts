6f1b6bb09319b80f7f5b172c08b2283efb60ed65
kyhuyho <hkhuy220802@gmail.com>
2024-01-26 11:27:05 +0700

    tom

--- src/scripts/conditional.ts
+++ src/scripts/conditional.ts
@@ -6,7 +6,19 @@ const isTrue = number >= 100 ? true : false;
 type Check<T> = T extends string ? "string" : "non-string";
 export type IsString = Check<string>;
 export type IsNonString = Check<number>;
+// Parameters: trả ra 1 mảng các tham số trong function
 function logObject(x: number, y: string) {}
 type Z = Parameters<typeof logObject>;
+// Extract
 type X = Extract<string | string[], string[]>; // string[]
-type Y = Exclude<string | string[], string[]>; // string
+// Extract
+type Y = Extract<string | string[], string[]>; // string
+// ReturnType: return type của function
+function sum(a: number, b: number): number {
+  return a + b;
+}
+function logMyName(): string {
+  return "Heros";
+}
+type SumF = ReturnType<typeof sum>;
+type LogMyNameF = ReturnType<typeof logMyName>;
